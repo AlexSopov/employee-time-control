78eab70ada6738411634b06ead9e2c0e95f7b9a0
Alex Sopov <sopov.alex.a@gmail.com>
2017-12-19 22:01:25 +0200

    Add test xUnit test

--- Tests/EmployeeTimeControl.Data.Test/UnitTest1.cs
+++ Tests/EmployeeTimeControl.Data.Test/UnitTest1.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Xunit;
 
 namespace EmployeeTimeControl.Data.Test
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void PassingTest()
         {
-            Assert.IsTrue(true);
+            Assert.Equal(4, 2 + 2);
         }
     }
 }

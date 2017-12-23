daa8546b410e82e146ef698681cd41a5091d6358
Alex Sopov <sopov.alex.a@gmail.com>
2017-12-23 08:32:22 +0200

    Add service to appveyour.yaml

--- Tests/EmployeeTimeControl.Data.Test/CardAccessTest.cs
+++ Tests/EmployeeTimeControl.Data.Test/CardAccessTest.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using EmployeeTimeControl.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace EmployeeTimeControl.Data.Test
 {
-    class CardAccessTest : DataTestBase
+    public class CardAccessTest : DataTestBase
     {
+       
     }
 }
--- Tests/EmployeeTimeControl.Data.Test/CardTest.cs
+++ Tests/EmployeeTimeControl.Data.Test/CardTest.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EmployeeTimeControl.Data.Models;
+using Xunit;
 
 namespace EmployeeTimeControl.Data.Test
 {
-    class CardTest
+    public class CardTest : DataTestBase
     {
+        [Fact]
+        public void CardNavigationPropertiesWork()
+        {
+            Card card = new Card()
+            {
+                Employee = new Employee()
+                {
+                    FirstName = "FirstName",
+                    SecondName = "SecondName",
+                    JobTitle = "Job",
+                    VisitingRule = new VisitingRule() { Description = "Description" }
+                },
+            };
+
+            employeeTimeControlDataContext.CardSet.Add(card);
+            Assert.NotEqual(0, employeeTimeControlDataContext.SaveChanges());
+
+            card = employeeTimeControlDataContext.CardSet.Find(card.CardId);
+
+            Assert.NotNull(card);
+            Assert.NotNull(card.Employee);
+            Assert.Equal("SecondName", card.Employee.SecondName);
+
+        }
     }
 }

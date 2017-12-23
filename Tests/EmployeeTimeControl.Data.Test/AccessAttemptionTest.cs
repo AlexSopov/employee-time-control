d9203f83e4dc309229f76de92728794b2d56c1c4
Alex Sopov <sopov.alex.a@gmail.com>
2017-12-23 08:25:06 +0200

    Add data test structure

---
+++ Tests/EmployeeTimeControl.Data.Test/AccessAttemptionTest.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeTimeControl.Data.Models;
+using System;
+using Xunit;
+
+namespace EmployeeTimeControl.Data.Test
+{
+    public class AccessAttemptionTest : DataTestBase
+    {
+        [Fact]
+        public void AccessAttemptionNavigationPropertiesWork()
+        {
+            AccessAttemption accessAttemption = new AccessAttemption()
+            {
+                Card = new Card()
+                {
+                    Employee = new Employee()
+                    {
+                        FirstName = "FirstName",
+                        SecondName = "SecondName",
+                        JobTitle = "Job",
+                        VisitingRule = new VisitingRule() { Description = "Description"}
+                    },
+                },
+                PassagePoint = new PassagePoint()
+                {
+                    Address = "Address"
+                },
+                SuccessStatus = true,
+                IsEnter = true,
+                AttemptionTime = DateTime.Now
+            };
+
+            employeeTimeControlDataContext.AccessAttemptionSet.Add(accessAttemption);
+            Assert.NotEqual(0, employeeTimeControlDataContext.SaveChanges());
+
+            accessAttemption = employeeTimeControlDataContext.AccessAttemptionSet.Find(accessAttemption.AccessAttemptionId);
+
+            Assert.NotNull(accessAttemption);
+            Assert.NotNull(accessAttemption.PassagePoint);
+            Assert.NotNull(accessAttemption.Card);
+        }
+    }
+}
---
+++ Tests/EmployeeTimeControl.Data.Test/DataTestBase.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeTimeControl.Data.AccessLayer;
+using System;
+
+namespace EmployeeTimeControl.Data.Test
+{
+    public class DataTestBase : IDisposable
+    {
+        public EmployeeTimeControlDataContext employeeTimeControlDataContext;
+
+        public DataTestBase()
+        {
+            employeeTimeControlDataContext = new EmployeeTimeControlDataContext("TestConnection");
+            CleanDatabase();
+        }
+
+        public void CleanDatabase()
+        {
+            employeeTimeControlDataContext.AccessAttemptionSet.RemoveRange(employeeTimeControlDataContext.AccessAttemptionSet);
+            employeeTimeControlDataContext.CardSet.RemoveRange(employeeTimeControlDataContext.CardSet);
+            employeeTimeControlDataContext.CardAccessSet.RemoveRange(employeeTimeControlDataContext.CardAccessSet);
+            employeeTimeControlDataContext.DayVisitingRuleSet.RemoveRange(employeeTimeControlDataContext.DayVisitingRuleSet);
+            employeeTimeControlDataContext.EmployeeSet.RemoveRange(employeeTimeControlDataContext.EmployeeSet);
+            employeeTimeControlDataContext.PassagePointSet.RemoveRange(employeeTimeControlDataContext.PassagePointSet);
+            employeeTimeControlDataContext.VisitingRuleSet.RemoveRange(employeeTimeControlDataContext.VisitingRuleSet);
+            employeeTimeControlDataContext.DayOffSet.RemoveRange(employeeTimeControlDataContext.DayOffSet);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    employeeTimeControlDataContext.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
